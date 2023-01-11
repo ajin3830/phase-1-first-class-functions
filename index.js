f1c975cd823e592f64c08093a978787aa5424590
ajin3830 <ajin3830@gmail.com>
2023-01-11 00:00:15 -0500

    completed lab

---
+++ index.js
@@ -0,0 +1,20 @@
+function receivesAFunction(callBackFunction) {
+    callBackFunction();
+}
+
+// function returnsANamedFunction() {
+//     return function newName() {
+//         return "Hello World";
+//     }
+// }
+
+function returnsAnAnonymousFunction() {
+    return function () {
+    }
+}
+
+const returnsANamedFunction = () => {
+    return newName = () => {
+
+    }
+}
